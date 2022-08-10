a3bcc4dd4aa18993b9a9ea70f4f777bd889ee9c6
benjamin kisenge <benkisenge03@gmail.com>
2022-08-10 19:13:18 +0200

    Create Js array of tasks

---
+++ src/index.js
@@ -0,0 +1,12 @@
+let task = [
+    {
+        description : "Drag 'n drop to reorder your list",
+        completed : false,
+        index : 0
+    },
+    {
+        description : "Drag 'n drop to reorder your list",
+        completed : false,
+        index : 0
+    }
+]
