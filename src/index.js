0b51caa50a65300c8dcea98bdce99dd62f6cfc80
benjamin kisenge <benkisenge03@gmail.com>
2022-08-10 19:59:17 +0200

    Create displayTasks function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-let task = [
+const Tasks = [
     {
         description : "Drag 'n drop to reorder your list",
         completed : false,
         index : 0
     },
     {
-        description : "Drag 'n drop to reorder your list",
+        description : "Manage all your lists in one place",
         completed : false,
-        index : 0
-    }
-]
+        index : 1
+    },
+    {
+        description : "Resync to clear out the old",
+        completed : false,
+        index : 3
+    }, 
+];
+const taskContainer = document.querySelector('.task-container');
+const taskTemplate = (task) => `
+    <div class="task">
+        <div class="t-left">
+            <input type="checkbox" name="Drag" class="checkbox"> 
+            <label for="">${task}</label>
+        </div>
+        <i class="fa-solid fa-ellipsis-vertical"></i> 
+    </div>
+`;
+const displayTasks = (arr) => {
+    arr.sort((a,b) => b.index - a.index);
+    arr.map(task => {
+        taskContainer.insertAdjacentHTML('afterbegin',taskTemplate(task.description));
+    });
+}
+displayTasks(Tasks); 
