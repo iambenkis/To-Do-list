4e09d08c8609a72cabe56c65e69892889edf1952
benjamin kisenge <benkisenge03@gmail.com>
2022-08-11 14:28:18 +0200

    Create template function

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -5,13 +5,29 @@ class myTodo {
         this.tasks = [];
     }
 
-    addTask (description){
+    addTask = (description) => {
         const myTask = {
             description,
             completed : false,
-            id : this.tasks.length + 1
+            index : this.tasks.length + 1
         }
         this.tasks.push(myTask)
-
     }
+    remove = (index) => {
+        this.tasks.forEach((task,id) => {
+            if(task.index === index) {
+                this.tasks
+                .splice(this.task[id],1);
+            }
+        });
+    }
+    template = (task) => `
+        <div class="task">
+            <div class="t-left">
+                <input type="checkbox" name="Drag" class="checkboxes"> 
+                <label for="" class="task-label">${task}</label>
+            </div>
+            <i class="fa-solid fa-ellipsis-vertical"></i> 
+        </div>
+    `;
 }
