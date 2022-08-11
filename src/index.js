ec324864391df5bb1afa616dea961e2e834b0314
benjamin kisenge <benkisenge03@gmail.com>
2022-08-11 02:28:29 +0200

    Debug webpack issue

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
+import './style.css'
 const Tasks = [
-    {
-        description : "Drag 'n drop to reorder your list",
-        completed : false,
-        index : 0
-    },
-    {
-        description : "Manage all your lists in one place",
-        completed : false,
-        index : 1
-    },
-    {
-        description : "Resync to clear out the old",
-        completed : false,
-        index : 3
-    }, 
+  {
+    description: "Drag 'n drop to reorder your list",
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Manage all your lists in one place',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Resync to clear out the old',
+    completed: false,
+    index: 2,
+  },
 ];
-const taskContainer = document.querySelector('.task-container');
+const taskContainer = document.querySelector('.task-container'); 
 const taskTemplate = (task) => `
     <div class="task">
         <div class="t-left">
@@ -26,9 +27,7 @@ const taskTemplate = (task) => `
     </div>
 `;
 const displayTasks = (arr) => {
-    arr.sort((a,b) => b.index - a.index);
-    arr.map(task => {
-        taskContainer.insertAdjacentHTML('afterbegin',taskTemplate(task.description));
-    });
-}
-displayTasks(Tasks); 
+  arr.sort((a, b) => b.index - a.index);
+  arr.map((task) => taskContainer.insertAdjacentHTML('afterbegin', taskTemplate(task.description)));
+};
+displayTasks(Tasks);
