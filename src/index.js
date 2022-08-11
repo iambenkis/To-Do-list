9351bf80b55e3243f48735ab2829625bbf5125a0
benjamin kisenge <benkisenge03@gmail.com>
2022-08-11 10:58:04 +0200

    Add addTask function in index.js

--- src/index.js
+++ src/index.js
@@ -4,20 +4,22 @@ const Tasks = [
   {
     description: "Drag 'n drop to reorder your list",
     completed: false,
-    index: 3,
+    index: 1,
   },
   {
     description: 'Manage all your lists in one place',
     completed: false,
-    index: 1,
+    index: 2,
   },
   {
     description: 'Resync to clear out the old',
     completed: false,
-    index: 2,
+    index: 3,
   },
 ];
 const taskContainer = document.querySelector('.task-container');
+const taskEntered = document.querySelector('.in-task');
+const submitTask = document.querySelector('.fa-left-long');
 const taskTemplate = (task) => `
     <div class="task">
         <div class="t-left">
@@ -27,8 +29,26 @@ const taskTemplate = (task) => `
         <i class="fa-solid fa-ellipsis-vertical"></i> 
     </div>
 `;
+
 const displayTasks = (arr) => {
   arr.sort((a, b) => b.index - a.index);
   arr.map((task) => taskContainer.insertAdjacentHTML('afterbegin', taskTemplate(task.description)));
 };
+
+const addTask = () => {
+  submitTask.addEventListener('click', (e) => {
+    if(taskEntered.value !== '') {
+      Tasks.unshift({
+        description: taskEntered.value,
+        completed: false,
+        index: Tasks.length + 1,
+      })
+      taskContainer.innerHTML = '';
+      displayTasks(Tasks); 
+      taskEntered.value = '';
+    }
+  })
+};
+
 displayTasks(Tasks);
+addTask();
