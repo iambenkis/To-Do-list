6c6c070a8137cd36b9d8a5533619095aa2b5fe63
benjamin kisenge <benkisenge03@gmail.com>
2022-08-12 14:07:04 +0200

    Delete console.logs

--- src/index.js
+++ src/index.js
@@ -20,103 +20,24 @@ const Tasks = [
 const taskContainer = document.querySelector('.task-container');
 const taskEntered = document.querySelector('.in-task');
 const submitTask = document.querySelector('.fa-left-long');
-// const deleteBtn  = document.querySelector('.delete');
-const form = document.querySelector('form');
-const taskTemplate = (task) => `
-    <div class="task">
-        <div class="t-left">
-            <input type="checkbox" name="Drag" class="checkboxes"> 
-            <label for="" class="task-label">${task}</label>
-        </div>
-        <i class="fa-solid fa-ellipsis-vertical"></i> 
-    </div>
-`;
-
-// class List {
-//   constructor(){
-//     this.myTask = [];
-//   }
-// }
-
-// const displayTasks = (arr) => {
-//   arr.sort((a, b) => b.index - a.index);
-//   arr.map((task) => taskContainer.insertAdjacentHTML('afterbegin', taskTemplate(task.description)));
-//   const taskLabel = taskContainer.querySelectorAll('.task-label');
-//   const checkboxes = taskContainer.querySelectorAll('.checkboxes');
-//   checkboxes.forEach((checkbox,index) => {
-//   checkbox.addEventListener("change", (e) => {
-//     if (e.target.checked) {
-//       taskLabel[index].classList.add('line');
-//     } else {
-//       taskLabel[index].classList.remove('line');
-//     }
-//   });
-// });
-//   deleteFunc();
-// };
-
-// const deleteFunc = () => {
-//   deleteBtn.addEventListener('click',(e)=> {
-//     e.preventDefault();
-//     const checkboxes = taskContainer.querySelectorAll('.checkboxes');
-//     checkboxes.forEach((checkbox,index) => {
-//         if (checkbox.checked){  
-//           Tasks.splice(index - 1, 1);
-//           console.log(Tasks , index);
-//           resetIndex();
-//           taskContainer.innerHTML = '';
-//           displayTasks(Tasks);  
-//           checkbox.checked = false;
-//         }
-//       });
-//   })
-// };
-
-// const addFunction = () => { 
-//   if(taskEntered.value !== '') {
-//     Tasks.unshift({
-//       description: taskEntered.value,
-//       completed: false,
-//       index: 1,
-//     });
-//     resetIndex();
-//     taskContainer.innerHTML = '';
-//     displayTasks(Tasks); 
-//     taskEntered.value = '';
-//   }
-// }
-
-// const resetIndex = () => {
-//   let initialIndex = 1;
-//     Tasks.forEach((task) => {
-//       task.index = initialIndex;
-//       initialIndex += 1;
-//     });
-// }
-
-// const addTask = () => {
-//   submitTask.addEventListener('click', (e) => {
-//     addFunction();
-//   })
-//   document.querySelector('form').addEventListener('submit', (e) => {
-//     addFunction();
-//   })
-// }; 
-// displayTasks(Tasks);
-// addTask();
+const form = document.querySelector('form'); 
 const todoList = new myTodo();
 todoList.displayTasks();
-submitTask.addEventListener('click', (e) => {
-  e.preventDefault();
+
+const confirmInput = () => {
   if (taskEntered.value.trim()) { 
     todoList.addTask(taskEntered.value);
     taskEntered.value = '';
-    taskContainer.innerHTML = '';
-    todoList.displayTasks(); 
-    console.log(todoList.tasks);
+    taskContainer.innerHTML = ''; 
+    todoList.displayTasks();  
   }
+}
+submitTask.addEventListener('click', (e) => {
+  e.preventDefault();
+  confirmInput();
 })
 
-document.addEventListener('DOMContentLoaded', () => { 
-  todoList.displayTasks();
-});
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  confirmInput(); 
+})
