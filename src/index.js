dc472334ba7843333ec33b7f895e87092dc6dc3d
benjamin kisenge <benkisenge03@gmail.com>
2022-08-12 14:14:43 +0200

    Include webpack

--- src/index.js
+++ src/index.js
@@ -1,43 +1,28 @@
-// import './style.css';
-import myTodo from "./modules/todo.js";
-const Tasks = [
-  {
-    description: "Drag 'n drop to reorder your list",
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Manage all your lists in one place',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Resync to clear out the old',
-    completed: false,
-    index: 2,
-  },
-];
+import './style.css';
+import MyTodo from './modules/todo.js';
+
 const taskContainer = document.querySelector('.task-container');
 const taskEntered = document.querySelector('.in-task');
 const submitTask = document.querySelector('.fa-left-long');
-const form = document.querySelector('form'); 
-const todoList = new myTodo();
+const form = document.querySelector('form');
+
+const todoList = new MyTodo();
 todoList.displayTasks();
 
 const confirmInput = () => {
-  if (taskEntered.value.trim()) { 
+  if (taskEntered.value.trim()) {
     todoList.addTask(taskEntered.value);
     taskEntered.value = '';
-    taskContainer.innerHTML = ''; 
-    todoList.displayTasks();  
+    taskContainer.innerHTML = '';
+    todoList.displayTasks();
   }
-}
+};
 submitTask.addEventListener('click', (e) => {
   e.preventDefault();
   confirmInput();
-})
+});
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  confirmInput(); 
-})
+  confirmInput();
+});
